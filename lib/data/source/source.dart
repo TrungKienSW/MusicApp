92059804c774f4b7f0a5ae3f9601e74f7a3bac23
TrungKienSW <kienpham06112002@gmail.com>
2026-03-14 12:20:56 +0700

    Add code to get data from internet

--- lib/data/source/source.dart
+++ lib/data/source/source.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import '../model/song.dart';
+import 'package:http/http.dart' as http;
 
 abstract interface class DataSource {
   Future<List<Song>?> loadData();
@@ -6,9 +9,23 @@ abstract interface class DataSource {
 
  class RemoteDataSoure implements DataSource {
   @override
-  Future<List<Song>?> loadData() {
-    // TODO: implement loadData
-    throw UnimplementedError();
+  Future<List<Song>?> loadData() async {
+      const url = 'https://thantrieu.com/resources/braniumapis/songs.json';
+      final uri = Uri.parse(url);
+      final response = await http.get(uri);
+      if(response.statusCode == 200){
+          final bodyContent  = utf8.decode(response.bodyBytes);
+          var songWrapper = jsonDecode(bodyContent) as Map;
+          var songList = songWrapper['songs'];
+          List<Song> songs = songList.map((song) => Song.fromJson(song)).toList();
+          return songs;
+
+      } else {
+        return null;
+      }
+
+
+
   }
 
  }
