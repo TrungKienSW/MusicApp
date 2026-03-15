836830003ed73571e61744e38ff8506b13d676c8
TrungKienSW <kienpham06112002@gmail.com>
2026-03-15 20:37:21 +0700

    Fix bug get json data from local

--- lib/data/repository/repository.dart
+++ lib/data/repository/repository.dart
@@ -11,19 +11,18 @@ class DefaultRepository implements Repository {
   final _remoteDataSource = RemoteDataSoure();
 
   @override
+
   Future<List<Song>?> loadData() async {
     List<Song> songs = [];
-    await _remoteDataSource.loadData().then((remoteSongs){
-      if(remoteSongs == null){
-        _localDataSource.loadData().then((localSongs){
-          if(localSongs != null){
-            songs.addAll(localSongs);
-          }
-        });
-      } else {
-        songs.addAll(remoteSongs);
+    final remoteSongs = await _remoteDataSource.loadData();
+    if (remoteSongs != null) {
+      songs.addAll(remoteSongs);
+    } else {
+      final localSongs = await _localDataSource.loadData();
+      if (localSongs != null) {
+        songs.addAll(localSongs);
       }
-    });
+    }
     return songs;
   }
 
