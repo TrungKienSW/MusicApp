163b3febd0018f73fe2fe5b09b772f69e8eadca2
TrungKienSW <kienpham06112002@gmail.com>
2026-03-15 21:26:38 +0700

    Add code for general ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:music_app/data/repository/repository.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'package:flutter/cupertino.dart';
+import 'package:music_app/ui/home/home.dart';
 
-  var repository = DefaultRepository();
-
-  debugPrint("Loading songs...");
-
-  var songs = await repository.loadData();
-
-  debugPrint("Songs loaded: ${songs?.length}");
-
-  if (songs != null) {
-    for (var song in songs) {
-      debugPrint(song.toString());
-    }
-  }
-}
-
-class MusicApp extends StatelessWidget {
-  const MusicApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+void main(){
+  runApp(const MusicApp());
 }
